3807c0bbee69b92bc6e1c3bbe39024c0dc7d9a4c
surya3997 <surya3997@gmail.com>
2017-10-15 20:04:13 +0530

    alteration in about function

--- include/print_funs.c
+++ include/print_funs.c
@@ -1,6 +1,6 @@
 void header()
 {
-	printf("\n\t\t\t _________________________________________________________________");
+    printf("\n\t\t\t _________________________________________________________________");
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|                                                                 |");
@@ -10,39 +10,27 @@ void header()
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|_________________________________________________________________|");
-    	
 }
 
 void about()
 {
-	
-	printf("\n\t\t\t _________________________________________________________________");
-	printf("\n\t\t\t|                                                                 |");
+
+    printf("\n\t\t\t _________________________________________________________________");
+    printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|    PROGRAMMER            :         S.SURYA PRASATH              |");
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|    VERSION NUMBER        :               1.0                    |");
     printf("\n\t\t\t|                                                                 |");
-  	printf("\n\t\t\t|    DESCRIPTION           :      # ORGANISES .MP3 FILES          |");
-  	printf("\n\t\t\t|                                    BASED ON ID3 TAG DATA.       |");
+    printf("\n\t\t\t|    DESCRIPTION           :      # ORGANISES .MP3 FILES          |");
+    printf("\n\t\t\t|                                    BASED ON ID3 TAG DATA.       |");
     printf("\n\t\t\t|                                 # FETCHES FILES FROM            |");
     printf("\n\t\t\t|                                    GIVEN DIRECTORY AND          |");
     printf("\n\t\t\t|                                      ITS SUB-DIRECTORIES.       |");
     printf("\n\t\t\t|                                 # SEARCHES ON THE BASIS         |");
     printf("\n\t\t\t|                                     OF TITLE ,ALBUM ,           |");
-	printf("\n\t\t\t|                                        ARTIST AND YEAR OF SONG  |");
-	printf("\n\t\t\t|                                 # PLAYS THE SELECTED FILE.      |");
-    printf("\n\t\t\t|                                                                 |");
-    printf("\n\t\t\t|    CONCEPTS USED          :     # ITERATION                     |");
-    printf("\n\t\t\t|                                 # ARRAYS                        |");
-    printf("\n\t\t\t|                                 # FUNCTIONS                     |");
-    printf("\n\t\t\t|                                 # NESTED FUNCTIONS              |");
-    printf("\n\t\t\t|                                 # RECURSIVE FUNTIONS            |");
-    printf("\n\t\t\t|                                 # POINTERS                      |");
-    printf("\n\t\t\t|                                 # FILES                         |");
-    printf("\n\t\t\t|                                 # DIRECTORIES                   |");
-    printf("\n\t\t\t|                                                                 |");
+    printf("\n\t\t\t|                                        ARTIST AND YEAR OF SONG  |");
+    printf("\n\t\t\t|                                 # PLAYS THE SELECTED FILE.      |");
     printf("\n\t\t\t|                                                                 |");
     printf("\n\t\t\t|_________________________________________________________________|");
 }
-
